029191e1b1202d3cf0d53b75f7c56e49d9eea619
Fernando de Almeida <100175464+FerAlmeida00@users.noreply.github.com>
2024-05-18 15:47:10 -0300

    Add files via upload

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,17 @@
+package primeiroprograma;
+
+/**
+ *
+ * @author Fe_Al
+ */
+public class PrimeiroPrograma {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+        System.out.println("Olá, Mundo!");
+    }
+    
+}
---
+++ ResolucaoSistema.java
@@ -0,0 +1,10 @@
+package resolucaosistema;
+import java.awt.Toolkit;
+import java.awt.Dimension;
+public class ResolucaoSistema{
+	public static void main(String[] args){
+		Toolkit tk = Toolkit.getDefaultToolkit();
+		Dimension d = tk.getScreenSize();
+		System.out.println("A resolução da tela é: " + d.width + " X " + d.height);
+	}
+}
